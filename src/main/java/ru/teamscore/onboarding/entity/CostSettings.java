dff73f936e4bf72fc1d1d9bc35901bcb44946cfd
CunningBird <68229355+CunningBird@users.noreply.github.com>
2024-01-18 18:02:38 +0300

    add appSettings

---
+++ src/main/java/ru/teamscore/onboarding/entity/CostSettings.java
@@ -0,0 +1,28 @@
+package ru.teamscore.onboarding.entity;
+
+import io.jmix.appsettings.defaults.AppSettingsDefault;
+import io.jmix.appsettings.entity.AppSettingsEntity;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@JmixEntity
+@Table(name = "COST_SETTINGS")
+@Entity
+public class CostSettings extends AppSettingsEntity {
+
+    @AppSettingsDefault("0")
+    @Column(name = "VAT")
+    private Double vat;
+
+    public void setVat(Double vat) {
+        this.vat = vat;
+    }
+
+    public Double getVat() {
+        return vat;
+    }
+
+
+}
--- src/main/java/ru/teamscore/onboarding/view/contract/ContractDetailView.java
+++ src/main/java/ru/teamscore/onboarding/view/contract/ContractDetailView.java
@@ -1,7 +1,11 @@
 package ru.teamscore.onboarding.view.contract;
 
+import io.jmix.appsettings.AppSettings;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import ru.teamscore.onboarding.entity.Contract;
 
+import ru.teamscore.onboarding.entity.CostSettings;
 import ru.teamscore.onboarding.view.main.MainView;
 
 import com.vaadin.flow.router.Route;
@@ -12,4 +16,13 @@ import io.jmix.flowui.view.*;
 @ViewDescriptor("contract-detail-view.xml")
 @EditedEntityContainer("contractDc")
 public class ContractDetailView extends StandardDetailView<Contract> {
+
+    @Autowired
+    private AppSettings appSettings;
+
+//    @PostConstruct
+//    public void flex() {
+//        Double vat = appSettings.load(CostSettings.class).getVat();
+//        System.out.println(vat);
+//    }
 }
